278b7bf1e1c40c379494dfe20a7d6720d74fc253
Ethan Salonga <ethansalonga28@gmail.com>
2024-11-11 22:07:11 -0500

    feat: Create Greeting component using Astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ethans-technical-writing-blog.netlify.app'
+  site: 'https://ethans-technical-writing-blog.netlify.app',
+  integrations: [preact()]
 })
